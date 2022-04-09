1f9158f52c70c88f46c3dea176cf8c7dcd0fdb13
Nuclear One <ajithsnair10@gmail.com>
2022-04-09 20:32:30 +0530

    Add logout Button

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,10 +1,22 @@
+import { useHistory } from 'react-router-dom';
 import './NavBar.css';
 
 const NavBar = ()=>{
+const history = useHistory();
+
+    const logoutHandler = (event)=>{
+        event.preventDefault();
+        localStorage.setItem('JWTTOKEN','');
+        localStorage.setItem('userID','');
+        localStorage.setItem('Email','');
+        history.replace('/auth');
+    }
+
     return(<div className='mainDivv'>
         <div className='subDivvH' >Home</div>
         <div className='subDivvP'>Products</div>
         <div className='subDivvA'>About us</div>
+        <div className='logoutDiv' > <button onClick={logoutHandler} className='logoutBtn' >LogOut</button> </div>
         <hr className='hrelement'></hr>
     </div>)
 }
